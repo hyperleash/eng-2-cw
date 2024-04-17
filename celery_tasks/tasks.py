64fc88588d6882b331f91d1463c511b3ac02537f
Cloud User <ec2-user@ip-10-0-6-234.eu-west-2.compute.internal>
2024-04-17 22:03:57 +0000

    tasks finalised

--- celery_tasks/tasks.py
+++ celery_tasks/tasks.py
@@ -16,28 +16,33 @@ def add(x, y):
 #     subprocess.run(command, shell=True, check=True)
 
 @app.task
-def run_prediction(file_name):
-    # Get file name without extension 
+def run_merizo(file_name):
+    
     # Get parent directory
     parent_dir = os.path.dirname(file_name)
     
+    # Get file name without extension 
     file_name_base = os.path.splitext(os.path.basename(file_name))[0]
 
     # Create output directory
     output_dir = os.path.join(parent_dir, file_name_base)
-    os.makedirs(output_dir, exist_ok=True)  # Create if it doesn't exist 
-
+    os.makedirs(output_dir, exist_ok=True)  
+    print("output_dir: ", output_dir)
+    print("parent_dir: ", parent_dir)
     # Run prediction
     command = f"python /home/ec2-user/data/Merizo/predict.py -d cpu -i {file_name} --iterate --save_domains"
     subprocess.run(command, shell=True, check=True)
 
     # Move files to the output directory
-    for filename in os.listdir():
+    print(f"os.listdir(parent_dir):  {os.listdir(parent_dir)}")
+    for filename in os.listdir(parent_dir):
         if filename.startswith(file_name_base):
+            print(f"GOT: {filename}")
+            source = os.path.join(parent_dir, filename)
             destination = os.path.join(output_dir, filename)
-            if os.path.exists(destination):  # Check if destination exists
+            if os.path.exists(destination):  # Check if exists and remove if so
                 os.remove(destination)
-            shutil.move(filename, output_dir)
+            shutil.move(source, output_dir)
 
     domain_files = []
     # Prepare domain files for pdb_tools (doesn't work with .dom_pdb extension)
@@ -49,6 +54,56 @@ def run_prediction(file_name):
             domain_files.append(os.path.join(output_dir, new_filename))
     
     return domain_files
+
+@app.task
+def run_pdb_centremass(file_name):
+    print("Running Centremass")
+    # Get parent directory
+    parent_dir = os.path.dirname(file_name)
+     # Get file name without extension 
+    file_name_base = os.path.splitext(os.path.basename(file_name))[0]
+
+    # Create output directory
+    output_dir = os.path.join(parent_dir, file_name_base)
+    os.makedirs(output_dir, exist_ok=True)  
+
+    # Run prediction
+    command = f"/home/ec2-user/data/pdbtools/scripts/pdb_centermass {file_name} > {output_dir}/centermass.txt"
+
+    subprocess.run(command, shell=True, check=True)
+
+@app.task
+def run_pdb_watercontact(file_name):
+    # Get parent directory
+    print("Running Watercontact")
+    print(file_name)
+    parent_dir = os.path.dirname(file_name)
+     # Get file name without extension 
+    file_name_base = os.path.splitext(os.path.basename(file_name))[0]
+
+    # Create output directory
+    output_dir = os.path.join(parent_dir, file_name_base)
+    os.makedirs(output_dir, exist_ok=True)  
+
+    # Run prediction
+    command = f"/home/ec2-user/data/pdbtools/scripts/pdb_watercontact {file_name} > {output_dir}/watercontact.txt"
+
+    subprocess.run(command, shell=True, check=True)
+    # for filename in os.listdir(output_dir):
+    #     print(filename)
+    #     if filename.startswith(file_name_base):
+    #         destination = os.path.join(output_dir, filename)
+    #         print(f"Destination: {destination}")
+    #         if os.path.exists(destination):  # Check if exists and remove if so
+    #             os.remove(destination)
+    #         shutil.move(filename, output_dir)
+
+    
+# @app.task
+# def run_pdb_watercontact(file_name):
+#/home/ec2-user/data/pdbtools/pdb_watercontact.py
+
+
 #start:
 # celery -A tasks worker --loglevel=INFO --pidfile=celery.pid --logfile=celery.log -D
 #stop:
--- celery_tasks/tasks_test.py
+++ celery_tasks/tasks_test.py
@@ -1,7 +1,44 @@
-from tasks import add, run_prediction
+from tasks import add, run_merizo, run_pdb_centremass, run_pdb_watercontact
+from celery import group
+import argparse
+import os
+
+parser = argparse.ArgumentParser(description='Process PDB files.', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument('-d','--directory_path', type=str, help='Path to the directory containing PDB files')
+args = parser.parse_args()
 
 result = add.delay(4, 5)  # Submit the task to the queue
 print(result.get())  # Wait for the result and print it (should output 9)
 
-result = run_prediction.delay('/home/ec2-user/data/shared/AF-A0A0A0MRZ7-F1-model_v4.pdb')
-print(result.get())  # Submit the task to the queue
+
+
+pdb_files = [os.path.join(args.directory_path, file) for file in os.listdir(args.directory_path) if file.endswith('.pdb')]
+print(pdb_files)
+
+merizo_job = group(run_merizo.s(file_path) for file_path in pdb_files)
+merizo_results = merizo_job.apply_async()
+
+paths = merizo_results.get()
+print(paths)
+
+domain_file_paths = [path for result in paths for path in result] 
+# directory_path = args.directory_path 
+
+# result = run_merizo.delay(args.directory_path)
+# domain_file_paths = result.get() # Submit the task to the queue
+# print(domain_file_paths) 
+
+
+# Assuming merizo_results contains lists of domain files produced by each task
+centermass_tasks = [run_pdb_centremass.s(path) for path in domain_file_paths]
+watercontact_tasks = [run_pdb_watercontact.s(path) for path in domain_file_paths]
+
+centermass_job = group(centermass_tasks) 
+
+centermass_result = centermass_job.apply_async()
+print(centermass_result.get())
+
+
+watercontact_job = group(watercontact_tasks)
+watercontact_result = watercontact_job.apply_async()
+print(watercontact_result.get())
