980ec91e0ef93e8f2a9fed62cdb384e5d66cdd61
Cloud User <ec2-user@ip-10-0-6-234.eu-west-2.compute.internal>
2024-04-21 22:39:47 +0000

    completed web-app

--- celery_tasks/tasks.py
+++ celery_tasks/tasks.py
@@ -1,18 +1,21 @@
 from celery import Celery
+from celery import group, chain
+
 import shutil
 import os
 import subprocess
+import zipfile
 
-app = Celery('tasks', broker='amqp://celery_worker:celery@10.0.15.70:5673//', backend='redis://10.0.15.70:6379')  # Replace 'rabbitmq_host' with your RabbitMQ hostname or IP
-app.conf.task_prefetch_count = 2
+app = Celery('tasks', broker='amqp://celery_worker:celery@10.0.15.70:5673//', backend='redis://10.0.15.70:6379')
+app.conf.task_prefetch_count = 1
 
 @app.task
 def add(x, y):
     return x + y 
 
-@app.task
+@app.task(queue='merizo_queue')
 def run_merizo(file_name):
-    
+    print("Running Merizo")
     # Get parent directory
     parent_dir = os.path.dirname(file_name)
     
@@ -22,14 +25,11 @@ def run_merizo(file_name):
     # Create output directory
     output_dir = os.path.join(parent_dir, file_name_base)
     os.makedirs(output_dir, exist_ok=True)  
-    print("output_dir: ", output_dir)
-    print("parent_dir: ", parent_dir)
     # Run prediction
     command = f"/home/ec2-user/data/celery_venv/bin/python /home/ec2-user/data/Merizo/predict.py -d cpu -i {file_name} --iterate --save_domains"
     subprocess.run(command, shell=True, check=True)
 
     # Move files to the output directory
-    print(f"os.listdir(parent_dir):  {os.listdir(parent_dir)}")
     for filename in os.listdir(parent_dir):
         if filename.startswith(file_name_base):
             print(f"GOT: {filename}")
@@ -45,12 +45,12 @@ def run_merizo(file_name):
         if filename.endswith('.dom_pdb'):
             new_filename = filename[:-8] + '.pdb' 
             os.rename(os.path.join(output_dir, filename), 
-                      os.path.join(output_dir, new_filename))
+                    os.path.join(output_dir, new_filename))
             domain_files.append(os.path.join(output_dir, new_filename))
     
     return domain_files
 
-@app.task
+@app.task(queue='pdb_queue')
 def run_pdb_centremass(file_name):
     print("Running Centremass")
     # Get parent directory
@@ -67,7 +67,7 @@ def run_pdb_centremass(file_name):
 
     subprocess.run(command, shell=True, check=True)
 
-@app.task
+@app.task(queue='pdb_queue')
 def run_pdb_watercontact(file_name):
     # Get parent directory
     print("Running Watercontact")
@@ -85,19 +85,104 @@ def run_pdb_watercontact(file_name):
 
     subprocess.run(command, shell=True, check=True)
 
-@app.task
+
+@app.task()
+def process_results(merizo_results):
+    domain_file_paths = merizo_results#[path for result in merizo_results]
+    
+    centermass_tasks = group(run_pdb_centremass.s(path) for path in domain_file_paths)
+    watercontact_tasks = group(run_pdb_watercontact.s(path) for path in domain_file_paths)
+
+    pdb_group = group(centermass_tasks, watercontact_tasks) 
+    return pdb_group.apply_async(routing_key="pdb_queue")
+
+@app.task()
 def run_pipeline(directory_path):
-    pdb_files = [os.path.join(args.directory_path, file) for file in os.listdir(args.directory_path) if file.endswith('.pdb')]
+    print("Running Pipeline")
+    unzipped_folder_name = ""
+    with zipfile.ZipFile(os.path.join(directory_path, 'data.zip'), 'r') as zip_ref:
+        for info in zip_ref.infolist():
+            if info.is_dir():  # Find the top-level directory
+                unzipped_folder_name = info.filename
+                break 
+    
+        zip_ref.extractall(directory_path)
+
+    os.remove(os.path.join(directory_path, 'data.zip')) # Remove original archive
+
+    directory_path = os.path.join(directory_path, unzipped_folder_name) # Update directory path to the unzipped folder
+
+    pdb_files = [os.path.join(directory_path, file) for file in os.listdir(directory_path) if file.endswith('.pdb')]
+
+    # Create a chain for each PDB file
+    file_processing_chains = [
+        chain(
+            run_merizo.s(file_path).set(routing_key='merizo_queue') | 
+            process_results.s()
+        )
+         for file_path in pdb_files
+    ]
+
+    grouped_chains = group(file_processing_chains)
+
+    task_chain = chain(grouped_chains , clean_up.s(directory_path=directory_path))
+    chain_result = task_chain.apply_async()
+    return chain_result
+
+
+@app.task()
+def clean_up(results, directory_path):
+    base_dir_name = os.path.basename(os.path.dirname(directory_path)).strip('/')
+    print(base_dir_name)
+    parent_dir = os.path.dirname(os.path.dirname(directory_path))
+    new_zip_path = os.path.join(parent_dir, 'data.zip')
+
+    shutil.make_archive(directory_path, 'zip', directory_path)
+    original_zip = os.path.join(parent_dir, base_dir_name + '.zip')
+    
+    os.rename(original_zip, new_zip_path)
+
+    shutil.rmtree(directory_path)  # Clean up the resutls directory
+
+@app.task()
+def run_pipeline_old(directory_path):
+    print("Running Pipeline")
+    # Unzip the data archive
+    unzipped_folder_name = ""
+    with zipfile.ZipFile(os.path.join(directory_path, 'data.zip'), 'r') as zip_ref:
+        for info in zip_ref.infolist():
+            if info.is_dir():  # Find the top-level directory
+                unzipped_folder_name = info.filename
+                break 
+    
+        zip_ref.extractall(directory_path)
+
+    os.remove(os.path.join(directory_path, 'data.zip')) # Remove original archive
+
+    directory_path = os.path.join(directory_path, unzipped_folder_name) # Update directory path to the unzipped folder
+
+    pdb_files = [os.path.join(directory_path, file) for file in os.listdir(directory_path) if file.endswith('.pdb')]
 
     merizo_job = group(run_merizo.s(file_path) for file_path in pdb_files)
-    merizo_results = merizo_job.apply_async()
+    merizo_results = merizo_job.apply_async(routing_key = "merizo_queue")
     paths = merizo_results.get()
 
     domain_file_paths = [path for result in paths for path in result] 
 
-    centermass_tasks = [run_pdb_centremass.s(path) for path in domain_file_paths]
-    watercontact_tasks = [run_pdb_watercontact.s(path) for path in domain_file_paths]
+    centermass_tasks = group(run_pdb_centremass.s(path) for path in domain_file_paths)
+    watercontact_tasks = group(run_pdb_watercontact.s(path) for path in domain_file_paths)
 
     pdb_job = group(centermass_tasks, watercontact_tasks)
-    pdb_results = pdb_job.apply_async()
+    pdb_results = pdb_job.apply_async(routing_key = "pdb_queue")
     pdb_results.get()
+
+    shutil.make_archive(directory_path, 'zip', directory_path)  # Zip the output directory
+
+    #shutil.rmtree(directory_path)  # Clean up the resutls directory
+
+
+
+#start:
+# celery -A tasks worker --loglevel=INFO --pidfile=celery.pid --logfile=celery.log -D
+#stop:
+# celery multi stop_verify worker --pidfile=celery.pid --logfile=celery.log
--- web_app/app.py
+++ web_app/app.py
@@ -5,29 +5,43 @@ import os
 import sys
 sys.path.append('../shared')
 from tasks import add, run_pipeline
+from tasks import app as celery_app
+from flask import send_from_directory
+from celery.result import AsyncResult
+import redis
+import shutil
 
 app = Flask(__name__)
 
 app.config['UPLOAD_FOLDER'] = '/home/ec2-user/data/shared/submissions'  # Set upload destination
 
+redis_client = redis.Redis(host='10.0.15.70', port=6379) 
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         file = request.files['zipfile']
-        if file:  # Check if file was provided
+        if file:  # Check the provided file 
             filename = file.filename
             if not filename.lower().endswith('.zip'):
-                print("Problem")
+                print("Non-archive uppload: ignoring")
                 return render_template('index.html', error="Please only upload .zip files.")
 
-            job_hash = str(uuid.uuid4()) # Generate unique hash
-            upload_path = os.path.join(app.config['UPLOAD_FOLDER'], job_hash)
-            os.makedirs(upload_path, exist_ok=True)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], 'data.zip'))
 
-            file.save(os.path.join(upload_path, 'data.zip'))
+            num_jobs = 2000
 
-            result = add.delay(10, 2)
-            print(result.get())
+            for _ in range(num_jobs):
+                job_hash = str(uuid.uuid4()) # Generate unique hash
+                upload_path = os.path.join(app.config['UPLOAD_FOLDER'], job_hash)
+                os.makedirs(upload_path, exist_ok=True)
+
+                #file.save(os.path.join(upload_path, 'data.zip'))
+                shutil.copyfile(os.path.join(app.config['UPLOAD_FOLDER'], 'data.zip'), os.path.join(upload_path, 'data.zip'))
+                result = run_pipeline.delay(upload_path)
+
+                task_id = result.id
+                redis_client.set(job_hash, task_id)
 
             return redirect(url_for('upload_success', job_hash=job_hash))
     else:
@@ -37,5 +51,39 @@ def index():
 def upload_success(job_hash):
     return render_template('success.html', job_hash=job_hash)
 
+@app.route('/download-results/<job_hash>')
+def download_results(job_hash):
+    result_archive_path = os.path.join(app.config['UPLOAD_FOLDER'], job_hash, 'data.zip')
+
+    if not os.path.exists(result_archive_path):
+        return render_template('error.html', error_message='Result archive not found')
+
+    return send_from_directory(os.path.dirname(result_archive_path), os.path.basename(result_archive_path), as_attachment=True)
+# Status route
+@app.route('/task-status/<job_hash>')
+def get_task_status(job_hash):
+    task_id = redis_client.get(job_hash).decode('utf-8')
+    if task_id:
+        chain_result = AsyncResult(task_id, app=celery_app)
+        child_task_statuses = []
+        print(chain_result.children)
+        for group_result in chain_result.children:  # Iterate over GroupResult objects
+            for child_result in group_result.children:  # Iterate over AsyncResult objects
+                child_task_statuses.append(child_result.status) # Get status
+        
+        if all(status == 'SUCCESS' for status in child_task_statuses): #if all succeed
+            overall_status = 'READY'
+        elif any(status == 'FAILURE' for status in child_task_statuses): # if any fail
+            overall_status = 'FAILED'
+        else:
+            overall_status = 'IN PROGRESS'
+    
+        if overall_status == 'READY':
+            return render_template('status.html', job_hash=job_hash, job_status=overall_status, download_ready=True)  
+        else:
+            return render_template('status.html', job_hash=job_hash, job_status=overall_status, download_ready=False)
+    else:
+        return render_template('error.html', error_message='Hash not found') 
+
 if __name__ == '__main__':
-    app.run(debug=True) 
+    app.run(debug=True)
